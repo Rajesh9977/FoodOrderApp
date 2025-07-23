0931631d8f9a60bbb4216f4514b6ef30e3a519dc
Rajes <rajeshrao7700@gmail.com>
2025-07-23 06:37:59 +0530

    New Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <h1 className="text-4xl">Hello</h1>
+    </div>
+  )
+}
+
+export default App
